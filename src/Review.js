a921fb6f60859546cd24eac8839fe25485dfd0bf
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-15 17:35:23 +0100

    iniial commit

--- src/Review.js
+++ src/Review.js
@@ -1,9 +1,36 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+console.log(people);
 
 const Review = () => {
-  return <h2>review component</h2>;
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img
+          className="person-img"
+          src={people[0].image}
+          alt={people[0].name}
+        />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{people[0].name}</h4>
+      <p className="job">{people[0].job}</p>
+      <p className="info">{people[0].text}</p>
+      <div className="button-container">
+        <button className="prev-btn">
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn">
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn">suprise me</button>
+    </article>
+  );
 };
 
 export default Review;
